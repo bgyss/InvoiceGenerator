78e241765d9a01a320e95954672ff0c1ac4cb83e
Brian Gyss <brian.gyss@gmail.com>
2013-11-26 10:25:20 -0500

    Changed PIL to Pillow in setup.py.

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ import InvoiceGenerator
 version = InvoiceGenerator.__versionstr__
 
 # release a version, publish to GitHub and PyPI
-if sys.argv[-1] == 'publish':
-    command = lambda cmd: subprocess.check_call(shlex.split(cmd))
-    command('git tag v' + version)
-    command('git push --tags origin master:master')
-    command('python setup.py sdist upload')
-    sys.exit()
+#if sys.argv[-1] == 'publish':
+#    command = lambda cmd: subprocess.check_call(shlex.split(cmd))
+#    command('git tag v' + version)
+#    command('git push --tags origin master:master')
+#    command('python setup.py sdist upload')
+#    sys.exit()
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -23,12 +23,12 @@ def read(fname):
 setup(
     name = "InvoiceGenerator",
     version =version,
-    author = "Adam Strauch",
-    author_email = "cx@initd.cz",
-    description = ("Library to generate PDF invoice."),
+    author = "Brian Gyss",
+    author_email = "brian@fizzmint.com",
+    description = ("Library to generate PDF invoice. Fork by bgyss."),
     license = "BSD",
     keywords = "invoice invoices generator",
-    url = "https://github.com/creckx/InvoiceGenerator",
+    url = "https://github.com/bgyss/InvoiceGenerator",
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     long_description="This is library to generate simple PDF invoice. It's based on ReportLab.",
     classifiers=[
@@ -37,7 +37,7 @@ setup(
         "License :: OSI Approved :: BSD License",
     ],
     install_requires=[
-        "reportlab", "PIL", "qrplatba>=0.3.3"
+        "reportlab", "Pillow", "qrplatba>=0.3.3"
         ],
     package_data={'InvoiceGenerator': ['locale/*/LC_MESSAGES/*']},
 )
